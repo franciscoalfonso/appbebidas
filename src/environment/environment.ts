fac6b8c2c089adba7d561d690dc25a01835eed9a
Francisco Alfonso Martínez Rodriguez <franciscoalfonso.martinez@outlook.com>
2018-06-29 14:38:03 -0500

    bebidas

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+     apiKey: "AIzaSyAoF-uSh1Qz9G5vx5uth0hk8Ut4WSbGToQ",
+        authDomain: "apprestaurante-558a9.firebaseapp.com",
+        databaseURL: "https://apprestaurante-558a9.firebaseio.com",
+        projectId: "apprestaurante-558a9",
+        storageBucket: "",
+        messagingSenderId: "397835798820"
+    }
+  };
